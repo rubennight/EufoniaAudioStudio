11a7dda3a6ff23289187c80435d79602f529040a
Rubén J. Navarro <133140934+rubennight@users.noreply.github.com>
2024-08-29 12:10:52 -0600

    Se agrega vista.

--- src/pages/ServiciosPage.tsx
+++ src/pages/ServiciosPage.tsx
@@ -1,4 +1,6 @@
-    import { motion } from 'framer-motion';
+    import { Button, Divider, Input, TextField } from '@mui/material';
+import { motion } from 'framer-motion';
+import SectionPage from '../components/SectionPage';
 
 
     export default function ServiciosPage(){
@@ -6,25 +8,50 @@
         const width = window.innerWidth;
 
         return(
-            <motion.div style={{ 
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-                width: '100vw'
-                }}>
-                <motion.div 
-                    initial={{ y: height * 2 }}
-                    animate={{ y: 0 }}
-                    transition={{ type: 'spring', stiffness: 80, damping: 10 }}
-                    style={{
-                        boxShadow: "0 8px 16px rgba(0, 0, 0, 0.3)",
-                        borderRadius: 15,
-                        overflow: 'hidden'
-                    }}
-                    >
-                    <img src="https://i.imgur.com/zT9JUtZ.jpeg" style={{ height: height * 0.8, objectFit: 'cover', borderRadius: 15 }} />
-                </motion.div>
-            </motion.div>
+            <div>
+                <SectionPage>
+                    Hola
+                </SectionPage>
+                <motion.div style={{ 
+                    display: 'flex',
+                    justifyContent: 'space-evenly',
+                    alignItems: 'left',
+                    height: '100vh',
+                    width: '100vw'
+                    }}>
+                    <motion.div 
+                        initial={{ x: height * 2 }}
+                        animate={{ x: 0 }}
+                        transition={{ type: 'spring', stiffness: 80, damping: 10 }}
+                        style={{
+                            borderRadius: 15,
+                            overflow: 'hidden'
+                        }}
+                        >
+                        <img src="https://i.imgur.com/zT9JUtZ.jpeg" style={{ height: height, objectFit: 'cover', borderRadius: 15 }} />
+                    </motion.div>
+                    <motion.div
+                        initial={{ x: height * 2}}
+                        animate={{ x: 0 }}
+                        transition={{ type: 'spring', stiffness: 80, damping: 10 }}
+                        style={{
+                            borderRadius: 15,
+                            height: height * 0.8,
+                            width: width * 0.4,
+                            flexDirection: 'column'
+                        }}>
+                        <h2>Registrate</h2>
+                        <br />
+                        <br />
+                        <TextField variant='standard' label='Correo Electrónico' fullWidth={true} />
+                        <br />
+                        <br />
+                        <TextField variant='standard' label='Contraseña' type='password' fullWidth={true} />
+                        <br />
+                        <br />
+                        <Button fullWidth={true} variant='contained' sx={{ borderRadius: 5, height: height * 0.1 }}>Login</Button>
+                    </motion.div>
+                </motion.div>                
+            </div>
         )
     }
